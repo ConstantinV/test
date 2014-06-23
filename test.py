97317e74746f49962248cf42a403c74278fe3256
konstatin <konstantinkul@gmail.com>
2014-06-23 13:39:27 +0400

    this is 100 words of Hello!

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python2.7
+
+"""
+       author:  Konstantin Kuleshov
+creation date:
+last modified:
+
+  description:
+  
+       usage:
+  
+"""
+
+import os
+import subprocess
+import sys
+
+print 'Hello!'*100
+
